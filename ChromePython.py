8a6543d49e8f4119bc45eadd7e93cc0ad69a477a
zhuoergen <zelongw@163.com>
2018-11-11 15:03:58 +0800

    first commit

---
+++ ChromePython.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+browser = webdriver.Chrome()
+browser.get('https://www.weibo.com/login.php')
+elem = browser.find_element_by_id(id_='loginname')
+elem.send_keys('your_username')
+elem_psw = browser.find_element_by_name('your_password')
